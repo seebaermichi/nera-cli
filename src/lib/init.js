ffff2668593bda02815dae5934a38c6e12cb7b2c
Michael Becker <michael.becker@oetkerdigital.com>
2019-09-02 20:01:09 +0200

    Add closing quote at final status message

--- src/lib/init.js
+++ src/lib/init.js
@@ -12,10 +12,11 @@ async function cloneNera (dir) {
 
 export default async function init (dir) {
   const cwd = `./${dir}`
+  await execShell('echo "Cloning Nera from github:\n--------------------"', { cwd })
   await cloneNera(dir)
   await del([`${cwd}/.git`])
   await execShell('echo "Install node modules:\n--------------------" && npm i', { cwd })
   await execShell('echo "Render page:\n--------------------" && npm run render', { cwd })
 
-  console.log('\x1b[32m', `Nera project "${dir} initialized!`)
+  console.log('\x1b[32m', `Nera project "${dir}" initialized!`)
 }
